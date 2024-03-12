78802dc6e521780fa015946a9a651c639790e90d
Collen <damd819@gmail.com>
2024-03-12 17:47:48 +0200

    add if

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    print("{:02}, ".format(number), end='')
-print(99)
+for number in range(0, 100):
+    if number == 99:
+        print(99)
+    else:
+        print("{:02}, ".format(number), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 89):
-    print("{:02}, ".format(number), end='')
-print(89)
+for num in range(0, 9):
+    for num2 in range (num, 10):
+        if str(num)+str(num2) == '89':
+            print('89')
+        elif num != num2:
+            print("{}, ".format(str(num)+str(num2)), end='')
