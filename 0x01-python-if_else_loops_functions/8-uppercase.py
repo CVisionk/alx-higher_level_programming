dec50fc7fe3691ce63f8370467e3b825d6338c9f
Collen <damd819@gmail.com>
2024-03-12 18:55:40 +0200

    fix style

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(s):
     for i in s:
-        temp = i;
+        temp = i
         if ord(i) >= 97 and ord(i) <= 122:
-            temp  = chr(ord(i) - 32)
+            temp = chr(ord(i) - 32)
         print('{}'.format(temp), end='')
     print('')
-uppercase("Best School 98 Battery street")
