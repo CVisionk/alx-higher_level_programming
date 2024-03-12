4c8374161ecfb0cf5074ab923f877b6934f34476
Collen <damd819@gmail.com>
2024-03-12 17:50:19 +0200

    pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 9):
-    for num2 in range (num, 10):
-        if str(num)+str(num2) == '89':
+    for num2 in range(num, 10):
+        if str(num) + str(num2) == '89':
             print('89')
         elif num != num2:
             print("{}, ".format(str(num)+str(num2)), end='')
