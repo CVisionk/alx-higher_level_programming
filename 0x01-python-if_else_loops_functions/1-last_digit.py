54e7fa75cfac83a18590b6ebab77c44e8dbff677
Collen <damd819@gmail.com>
2024-03-12 15:15:54 +0200

    fixed type

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ lastDigit = 0
 if (number != '0'):
     lastDigit = int(int(number[-1])*int(number)/abs(int(number)))
 if lastDigit == 0:
-    line = "is zero"
+    line = "is 0"
 elif lastDigit < 6:
     line = "is less than 6 and not 0"
 else:
     line = "is greater than 5"
-print(f"Last digit of {number} is {lastDigit} and {line}")
+print(f"Last digit of {int(number)} is {lastDigit} and {line}")
