b894f7bd1f10b1becf35a759edae8f2621354f1a
Collen <damd819@gmail.com>
2024-03-15 00:47:35 +0200

    hide

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in sort(names):
-        if not name.startswith("__"):
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
             print(name)
