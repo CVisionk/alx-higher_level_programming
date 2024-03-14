cd75ac27d29e554caab7bf28d1971ae2f5a87183
Collen <damd819@gmail.com>
2024-03-15 00:45:34 +0200

    hiddenfix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    for name in dir(hidden_4):
+    names = dir(hidden_4)
+    for name in sort(names):
         if not name.startswith("__"):
             print(name)
