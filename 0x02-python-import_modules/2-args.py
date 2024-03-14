113da4fca5054db447b0d4e12f93f418f6d64636
Collen <damd819@gmail.com>
2024-03-14 23:26:56 +0200

    fix args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     num_args = len(sys.argv)-1
-    if len(sys.argv) == 1:
+    if num_args == 0:
         print("{} arguments.".format(num_args))
     else:
         print("{} arguments:".format(num_args))
