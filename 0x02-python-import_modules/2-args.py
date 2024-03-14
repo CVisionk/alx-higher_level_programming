8df47d7946f40a614320bb5cf329c45d93669930
Collen <damd819@gmail.com>
2024-03-14 23:25:12 +0200

    add variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments.".format(len(sys.argv)-1))
+    num_args = len(sys.argv)-1
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(num_args))
+    else:
+        print("{} arguments:".format(num_args))
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{} : {}".format(i, arg))
