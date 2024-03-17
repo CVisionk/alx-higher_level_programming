cfe824b07f79121d84a9199617d75c57879dc6b4
Collen <damd819@gmail.com>
2024-03-17 14:38:18 +0200

    5-no_c.py2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,4 @@
 def no_c(my_string):
     my_string = ''.join(my_string.split('c'))
     my_string = ''.join(my_string.split('C'))
-    return mystring
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+    return my_string
