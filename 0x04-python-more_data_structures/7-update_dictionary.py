a72e74fdb642fa855cfe0e38d6132a872eb4f591
Collen <damd819@gmail.com>
2024-03-20 19:29:49 +0200

    7-update_dictionary.py2

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
+    item = {key: value}
+    a_dictionary.update(item)
     return a_dictiony
