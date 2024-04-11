97ed871e1b66e7d46c062438cce46af84b6b2f16
Collen <damd819@gmail.com>
2024-04-11 13:45:45 +0200

    fix betty and pycodestyle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,14 +8,16 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t* current = list;
-        listint_t* faster = list;
-	if(!list) return 0;
-	while(current != NULL && faster != NULL)
+	listint_t *current = list;
+	listint_t *faster = list;
+
+	if (!list)
+		return (0);
+	while (current != NULL && faster != NULL)
 	{
 		current = current->next;
 		faster = faster->next->next;
-		if(current == faster)
+		if (current == faster)
 		{
 			return (1);
 		}
