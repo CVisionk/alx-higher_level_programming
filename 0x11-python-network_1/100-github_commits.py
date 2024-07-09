1ac4c626cda8b63a49bb34e3c5c872a5ab09ea77
Collen <damd819@gmail.com>
2024-07-10 01:00:02 +0200

    added 6- 7- 8- ...

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """
+    main
+    """
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    response = requests.get(url)
+    commits = response.json()
+    
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author}")
