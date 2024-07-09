f499967dbf01fc00d688cb9abd40914aefc55369
Collen <damd819@gmail.com>
2024-07-09 19:16:13 +0200

    Created 0-hbtn_status.py 1-hbtn_header.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 import sys
 import urllib
 
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
-   #status = response.sta
-
-print("Body response:")
-print(f"    - type: {type(html)}")
-print(f"    - content: {html}")
-print(f"    - utf8 content: {html.decode('utf-8')}")
+if __name__ == "__main__":
+    """
+    main: entry.
+    """
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:")
+    print(f"    - type: {type(html)}")
+    print(f"    - content: {html}")
+    print(f"    - utf8 content: {html.decode('utf-8')}")
